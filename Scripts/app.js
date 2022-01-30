64ea58ca238bf8ebcf70248260c9e0e9579dafd5
Ankitjp01 <apatil23@my.centennialcollege.ca>
2022-01-30 23:17:49 +0530

    first commit

---
+++ Scripts/app.js
@@ -0,0 +1,21 @@
+(function(){
+
+function Start()
+{
+    let myVariable = 20;
+    console.log("my number is: " + myVariable);
+
+    for(let index = 0; index< myVariable; index++)
+    console.log("your number is " + index);
+
+
+
+}
+
+
+
+
+})();
+
+const myVariable = 10;
+console.log("my number is: " + myVariable);
